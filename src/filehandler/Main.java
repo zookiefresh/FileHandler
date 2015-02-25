4dc61e9a53e7444ce4705d4c383e974d2d8a919a
Duy <doubled@yahoo.com>
2015-02-24 20:51:02 -0800

    Propagate exception up to user

---
+++ src/filehandler/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filehandler;
+
+import java.io.FileNotFoundException;
+
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args){
+        // let the 'user' or main handle the exception
+        FileHandler fh = new FileHandler();
+        String [] filenames = { "test.txt",
+                                "image.jpg",
+                                "video.mp4"
+                              };
+        for(String filename : filenames){
+            try{
+                fh.readFile(filename);
+            }
+            catch (FileNotFoundException e){
+                System.out.println("CRAP! file not found");            
+            }
+        }
+    }
+}
