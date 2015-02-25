1b679e96a0840ea7ecae303fbb573f96f2f3594a
Duy <doubled@yahoo.com>
2015-02-24 20:38:06 -0800

    Class Example complete

--- src/filehandler/FileHandler.java
+++ src/filehandler/FileHandler.java
@@ -5,10 +5,35 @@
  */
 package filehandler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  *
  * @author User
  */
-public class FileHandler {
+public class FileHandler  {
+    
+    FileHandler(){}
+    
+    void readFile(String filename){
+        System.out.println("Attempting to read " + filename);        
+        try{            
+            long filesize = getFileSize(filename);        
+            System.out.println(filename + " size is " + filesize);
+        }
+        catch (FileNotFoundException e){
+           System.out.println("File missing");           
+        }        
+    }
+    
+    long getFileSize(String filename) throws FileNotFoundException{
+        File file = new File(filename);
+        if(!file.exists()){
+            FileNotFoundException e = new FileNotFoundException(filename);
+            throw e;
+        }
+        return file.length();
+    }
     
 }
